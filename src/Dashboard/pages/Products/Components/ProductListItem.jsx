944649d20c700eae90c69c06f6b2eed693bcfb49
Matthias Greeven <matsgreeven@gmail.com>
2024-03-21 13:31:19 +0100

    Added wishlist functionality

--- src/Dashboard/pages/Products/Components/ProductListItem.jsx
+++ src/Dashboard/pages/Products/Components/ProductListItem.jsx
@@ -1,45 +1,68 @@
-import  './ProductListItem.css'
+import './ProductListItem.css'
 import { Link } from 'react-router-dom'
 import BasketIconSVG from '../../../../assets/BasketIconSVG'
 import PropTypes from "prop-types"
 import HeartIconSVG from '../../../../assets/HeartIconSVG'
 import { CartItem } from '../../../../Models/CartItem'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { DataContext } from '../../../../App'
+import { DeleteData, environment, PostData } from '../../../../Services/FetchService'
 
-function ProductListItem({data}) {
+function ProductListItem({ data }) {
   const urlLink = `/products/${data?.id}`
   const dataContext = useContext(DataContext)
+  const [wishlistItem, setWishListItem] = useState({})
 
   const handleAddCartSubmit = () => {
     let productIndex = dataContext.cart.findIndex(obj => obj.productId === data.id)
 
-    if(productIndex === -1) {
+    if (productIndex === -1) {
       // Add product to cart
-      dataContext.setCart([...dataContext.cart, {productId: data?.id, quantity: 1}])
+      dataContext.setCart([...dataContext.cart, { productId: data?.id, quantity: 1 }])
       console.log("Add product to cart: ");
       console.log("product Index: " + productIndex)
       console.log(dataContext.cart)
     } else {
       // Increase quantity of product in cart
       let newCart = [...dataContext.cart];
-      newCart[productIndex].quantity += 1;  
+      newCart[productIndex].quantity += 1;
       dataContext.setCart(newCart)
-      console.log("Add quantity of product in cart: ");
-      console.log(dataContext.cart)
     }
   }
+  const handleFavorite = async () => {
+    if (!dataContext.userData.id) {
+      alert('Please log in to add items to the wishlist.');
+      return;
+    }
+
+    console.log(dataContext.wishlist);
+    console.log(data)
+    const index = dataContext.wishlist.findIndex(item => {
+      return item.productId === data.id;
+    });
+    if (index === -1) {
+      console.log("adding")
+      // Item not in wishlist, add it
+      const response = await PostData(environment + `/wishlist/${data.id}`, {}, setWishListItem);
+    } else {
+      console.log("Deleting")
+      const wishlistItemId = dataContext.wishlist.find(item => item.productId === data.id).id;
+      // Item already in wishlist, remove it
+      await DeleteData(environment + `/wishlist/${wishlistItemId}`, setWishListItem);
+    }
+    dataContext.setUpdateWishlist(true)
+  };
 
   return (
     <div className='product'>
       <div className='product-container'>
         <div className='product-container-padding'>
           <div className='product-image-align'>
-            <button className='product-button-wishlist'> {/* TODO: Add functionallity*/}
-              <HeartIconSVG/>
+            <button className='product-button-wishlist' onClick={handleFavorite}> {/* TODO: add this product to the wishlist array in datacontext*/}
+              <HeartIconSVG />
             </button>
             <Link to={urlLink}>
-              <img className='image-container' src={data.imageURL}/>
+              <img className='image-container' src={data.imageURL} />
             </Link>
           </div>
           <Link to={urlLink} className='product-container-title'>
@@ -56,8 +79,8 @@ function ProductListItem({data}) {
   )
 }
 
-ProductListItem.propTypes = { 
-	data: PropTypes.object.isRequired
+ProductListItem.propTypes = {
+  data: PropTypes.object.isRequired
 }
 
 
